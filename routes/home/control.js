43007e7e72ba619ac26d5cb76aa0a7777544d7b7
남형석 <azerc@MacMini.local>
2022-06-17 17:50:02 +0900

    ....restructuring

--- routes/home/control.js
+++
@@ -1,8 +0,0 @@
-export const get = {
-  index: (req, res) => {
-    res.render(`home/index.ejs`);
-  },
-  login: (req, res) => {
-    res.render(`home/login.ejs`);
-  },
-};
--- routes/home/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { get } from "./control.js";
-
-const router = express.Router();
-
-router.get("/", get.index);
-router.get("/login", get.login);
-
-export default router;
