0213ac6425d48b2e98b7e8aabd93e087c6056b05
남형석 <azerc@MacMini.local>
2022-06-18 14:19:27 +0900

    save html object by DOM

--- src/public/js/home/login.js
+++ src/public/js/home/login.js
@@ -1,1 +1,13 @@
-console.log("🟢 Server started on port 3000");
+const id = document.querySelector("#id");
+const password = document.querySelector("#password");
+const loginBtn = document.querySelector("#loginBtn");
+
+function login() {
+  const req = {
+    id: id.value,
+    password: password.value,
+  };
+  console.log(req);
+}
+
+loginBtn.addEventListener("click", login);
